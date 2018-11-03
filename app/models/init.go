763d6c97bc6f1d15a5e5c1ede8ceeec5a6348c37
axiaoxin <254606826@qq.com>
2018-11-03 20:10:59 +0800

    overwrite Model to custon json field

--- app/models/init.go
+++ app/models/init.go
@@ -3,18 +3,23 @@
 package models
 
 import (
+	"time"
+
 	"github.com/axiaoxin/gin-skeleton/app/utils"
-	"github.com/jinzhu/gorm"
 	"github.com/sirupsen/logrus"
 )
 
-type BaseModel gorm.Model
+type BaseModel struct {
+	ID        uint       `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:createdAt`
+	UpdatedAt time.Time  `json:updatedAt`
+	DeletedAt *time.Time `json:-`
+}
 
 // Models save your models like &MODEL{} at there which will be auto migrate when server starting
 var Models = []interface{}{}
 
-// Migrate run AutoMigrate to create database tables by models
-// add your models
+// Migrate run AutoMigrate to create database tables which in Models array
 // running after InitGormDB
 func Migrate() {
 	if err := utils.DB.AutoMigrate(Models...).Error; err != nil {
