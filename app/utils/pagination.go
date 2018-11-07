9e41241f9fefc656cbf20f0a58c3cbdad650b4f1
axiaoxin <254606826@qq.com>
2018-11-07 19:09:10 +0800

    add paginate utils

---
+++ app/utils/pagination.go
@@ -0,0 +1,52 @@
+package utils
+
+import "math"
+
+type Pagination struct {
+	// 数据总数
+	ItemsCount int `json:"itemsCount"`
+	// 分页总数
+	PagesCount int `json:"pagesCount"`
+	// 当前页码
+	PageNum int `json:"pageNum"`
+	// 分页大小
+	PageSize int `json:"pageSize"`
+	// 是否有上一页
+	HasPrev bool `json:"hasPrev"`
+	// 是否有下一页
+	HasNext bool `json:"hasNext"`
+	// 上一页页码
+	PrevPageNum int `json:"prevPageNum"`
+	// 下一页页码
+	NextPageNum int `json:"nextPageNum"`
+}
+
+// Paginate 计算分页信息
+func Paginate(itemsCount, pageNum, pageSize int) Pagination {
+	if itemsCount <= 0 {
+		return Pagination{}
+	}
+	pagesCount := int(math.Ceil(float64(itemsCount) / float64(pageSize)))
+	hasNext := false
+	nextPageNum := pageNum
+	if pageNum+1 <= pagesCount {
+		hasNext = true
+		nextPageNum = pageNum + 1
+	}
+	hasPrev := false
+	prevPageNum := pageNum
+	if pageNum-1 > 0 {
+		hasPrev = true
+		nextPageNum = pageNum - 1
+	}
+	return Pagination{
+		ItemsCount:  itemsCount,
+		PagesCount:  pagesCount,
+		PageNum:     pageNum,
+		PageSize:    pageSize,
+		HasPrev:     hasPrev,
+		HasNext:     hasNext,
+		PrevPageNum: prevPageNum,
+		NextPageNum: nextPageNum,
+	}
+}
