2df03c28cc5b57494ee44524408279a4c0d6106b
ashinchen <ashinchen@tencent.com>
2018-11-10 18:59:22 +0800

    update jsontime function and add test

---
+++ app/utils/jsontime_test.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+)
+
+type S struct {
+	DT time.Time
+	JT JSONTime
+}
+
+func TestJSONTime(t *testing.T) {
+	s := S{}
+	now, _ := time.Parse(timeFormat, "2018-11-10 18:52:25.123")
+	s.DT = now
+	s.JT = JSONTime{Time: now}
+	j, _ := json.Marshal(s)
+	json.Unmarshal(j, s)
+	if s.JT.String() != "2018-11-10 18:52:25" {
+		t.Error("json time field format error")
+	}
+}
