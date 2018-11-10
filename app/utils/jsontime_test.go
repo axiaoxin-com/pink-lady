30979db45eec2a2202cf815dc4e2e6647c81af8c
axiaoxin <254606826@qq.com>
2018-11-10 19:56:55 +0800

    bugfix

--- app/utils/jsontime_test.go
+++ app/utils/jsontime_test.go
@@ -17,7 +17,10 @@ func TestJSONTime(t *testing.T) {
 	s.DT = now
 	s.JT = JSONTime{Time: now}
 	j, _ := json.Marshal(s)
-	json.Unmarshal(j, s)
+	err := json.Unmarshal(j, &s)
+	if err != nil {
+		t.Error(err)
+	}
 	if s.JT.String() != "2018-11-10 18:52:25" {
 		t.Error("json time field format error")
 	}
