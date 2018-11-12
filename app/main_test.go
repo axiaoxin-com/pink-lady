537c04180a250815a46f2c849f52294e1531448e
ashinchen <ashinchen@tencent.com>
2018-11-12 18:36:24 +0800

    delete test main

--- app/main_test.go
+++ app/main_test.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"os"
 	"testing"
 
 	"github.com/axiaoxin/gin-skeleton/app/utils"
-	"github.com/gin-gonic/gin"
 )
-
-func setup() {
-	gin.SetMode(gin.TestMode)
-}
-
-func teardown() {
-
-}
 
 func TestSetupAPP(t *testing.T) {
 	app := SetupAPP()
@@ -22,11 +12,4 @@ func TestSetupAPP(t *testing.T) {
 	if w.Result().StatusCode != 200 {
 		t.Error("SetupAPP fail")
 	}
-}
-
-func TestMain(m *testing.M) {
-	setup()
-	exitCode := m.Run()
-	teardown()
-	os.Exit(exitCode)
 }
