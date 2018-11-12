45225a197f64acdf7241e4d8520b515f39a4a7f1
axiaoxin <254606826@qq.com>
2018-11-12 12:32:51 +0800

    update errorhandler test

--- app/middleware/errorhandler_test.go
+++ app/middleware/errorhandler_test.go
@@ -5,7 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/axiaoxin/gin-skeleton/app/utils/response"
+	"git.code.oa.com/TencentZhiYun/zls/app/utils/response"
 	"github.com/gin-gonic/gin"
 )
 
@@ -41,6 +41,32 @@ func TestErrorHandler500(t *testing.T) {
 		t.Error(err)
 	}
 	if r.Code != 5 {
+		t.Error("json code error")
+	}
+}
+
+func TestErrorHandler400(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.JSON(400, 1)
+	ErrorHandler()(c)
+	if w.Body.String() != "1" {
+		t.Error("400 should not be handled")
+	}
+}
+
+func TestErrorHandler401(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.JSON(401, 1)
+	ErrorHandler()(c)
+	j := w.Body.Bytes()[1:] // 这里的测试方式有点问题，body中会保存c.JSON在前面返回的1，手动去掉
+	r := response.Response{}
+	err := json.Unmarshal(j, &r)
+	if err != nil {
+		t.Error(err)
+	}
+	if r.Code != 2 {
 		t.Error("json code error")
 	}
 }
