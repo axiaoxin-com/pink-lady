6d11c7a18573157e87dea01ecb7464cac8381a95
axiaoxin <254606826@qq.com>
2018-11-12 14:19:24 +0800

    add output param for InitLogrus  and add ginlogrus middleware test

---
+++ app/middleware/ginlogrus_test.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	"github.com/axiaoxin/gin-skeleton/app/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func TestGinLogrus(t *testing.T) {
+	type logStruct struct {
+		Path      string `json:"path"`
+		Method    string `json:"method"`
+		ClientIP  string `json:"clientIP"`
+		UserAgent string `json:"userAgent"`
+		RequestID string `json:"requestID"`
+		Status    int    `json:"status"`
+		Size      int    `json:"size"`
+		Latency   string `json:"latency"`
+	}
+	buf := &bytes.Buffer{}
+	ls := logStruct{}
+	utils.InitLogrus(buf, "debug", "json")
+	r := gin.New()
+	r.Use(RequestID()) // must get request id
+	r.Use(GinLogrus())
+	r.GET("/", func(c *gin.Context) {})
+	utils.PerformTestingRequest(r, "GET", "/")
+	err := json.Unmarshal(buf.Bytes(), &ls)
+	if err != nil {
+		t.Error(err)
+	}
+	if ls.Path != "/" || ls.Method != "GET" || ls.RequestID == "" || ls.Status != 200 {
+		t.Error("log field error ", ls)
+	}
+}
