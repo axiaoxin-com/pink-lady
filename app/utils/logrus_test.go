a9f1677b47af5019dad908bad247f57c9b15df26
axiaoxin <254606826@qq.com>
2018-11-12 14:24:30 +0800

    fix logrus test

--- app/utils/logrus_test.go
+++ app/utils/logrus_test.go
@@ -1,17 +1,18 @@
 package utils
 
 import (
+	"os"
 	"testing"
 
 	"github.com/sirupsen/logrus"
 )
 
 func TestInitLogrus(t *testing.T) {
-	InitLogrus("debug", "text")
+	InitLogrus(os.Stdout, "debug", "text")
 	if logrus.GetLevel().String() != "debug" {
 		t.Error("set level failure")
 	}
-	InitLogrus("error", "text")
+	InitLogrus(os.Stdout, "error", "text")
 	if logrus.GetLevel().String() != "error" {
 		t.Error("set level failure")
 	}
