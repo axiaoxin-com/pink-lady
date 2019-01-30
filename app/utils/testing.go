a410a51898b688ca2a6abc1decf746aef6c78ed8
ashinchen <ashinchen@tencent.com>
2019-01-30 16:23:31 +0800

    add https://github.com/benbjohnson/testing functions

--- app/utils/testing.go
+++ app/utils/testing.go
@@ -2,8 +2,13 @@ package utils
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"testing"
 
 	"github.com/alicebob/miniredis"
 	"github.com/sirupsen/logrus"
@@ -35,9 +40,35 @@ func MockRedis() (*miniredis.Miniredis, error) {
 	return s, err
 }
 
-
 // MockHTTPServer provide a mock http server for testing
 // Usage: https://gist.github.com/axiaoxin/aa8014738c6a02ce4e66eb01168d24fe
 func MockHTTPServer(f http.HandlerFunc) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(f))
+}
+
+// Assert fails the test if the condition is false.
+func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
+
+// OK fails the test if an err is not nil.
+func OK(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// Equals fails the test if exp is not equal to act.
+func Equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
+	}
 }
