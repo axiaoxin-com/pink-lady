6bb221bbc26fde395f93c585797c49db3c00c107
axiaoxin <254606826@qq.com>
2019-03-13 15:42:34 +0800

    fix delete demo not clear

--- misc/new-project.sh
+++ misc/new-project.sh
@@ -9,7 +9,7 @@ main() {
     gopath=`go env GOPATH`
     echo -e "${NOTICE_FLAG} New project will be create in ${WHITE}${gopath}/src/"
     echo -e "${NOTICE_FLAG} You should enter the project full name such like <github.com/username/projectname>"
-    echo -ne "${QUESTION_FLAG} ${CYAN}Enter your new project full name:${CYAN}]: "
+    echo -ne "${QUESTION_FLAG} ${CYAN}Enter your new project full name${CYAN}: "
     read projname
     projname_dir=`dirname ${projname}`
     mkdir -p ${gopath}/src/${projname_dir}
@@ -27,11 +27,11 @@ main() {
 
     # remove demo
     if [ "${rmdemo}" == "n" ] || [ "${rmdemo}" == "N" ]; then
-        rm -rf app/docs
         rm -rf app/apis/demo
         rm -rf app/services/demo
         rm -rf app/models/demo
         sed -i "/demo routes start/,/demo routes end/d" app/apis/routes.go
+        sed -i '/app\/apis\/demo"$/d' app/apis/routes.go
     fi
     echo -e "${NOTICE_FLAG} Create ${projname} succeed."
 
