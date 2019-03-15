20113902b5b8914274fa64312e671f27545b547f
ashinchen <ashinchen@tencent.com>
2019-03-15 22:32:56 +0800

    log middleware record full url and request body #12

--- app/middleware/ginlogrus.go
+++ app/middleware/ginlogrus.go
@@ -1,7 +1,10 @@
 package middleware
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"time"
 
 	"github.com/axiaoxin/pink-lady/app/utils"
@@ -9,20 +12,34 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+func readBody(reader io.Reader) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(reader)
+
+	s := buf.String()
+	return s
+}
+
 // GinLogrus is a logger middleware, which use the logrus replace gin default logger
 func GinLogrus() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
-		// some evil middlewares modify this values
-		path := c.Request.URL.Path
+
+		url := c.Request.URL.String()
+		buf, _ := ioutil.ReadAll(c.Request.Body)
+		rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
+		rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf)) //We have to create a new Buffer, because rdr1 will be read.
+		body := readBody(rdr1)
+		c.Request.Body = rdr2
 		c.Next()
 
 		end := time.Since(start)
 		status := c.Writer.Status()
 
 		entry := utils.Logger.WithFields(logrus.Fields{
-			"path":      path,
+			"url":       url,
 			"method":    c.Request.Method,
+			"body":      body,
 			"clientIP":  c.ClientIP(),
 			"userAgent": c.Request.UserAgent(),
 			"status":    status,
