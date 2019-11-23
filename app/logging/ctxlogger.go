94c06ad55d3212d93dd2301c2234d1403ba0f801
axiaoxin <254606826@qq.com>
2019-11-23 14:47:54 +0800

    mv requestid in here

--- app/logging/ctxlogger.go
+++ app/logging/ctxlogger.go
@@ -2,6 +2,7 @@ package logging
 
 import (
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 	"go.uber.org/zap"
 )
 
@@ -14,18 +15,48 @@ const (
 
 // SetCtxLogger add a logger with given field into the gin.Context
 // and set requestid field get from context
-func SetCtxLogger(c *gin.Context, fields ...zap.Field) {
-	ctxLogger := CloneLogger()
-	ctxLogger = ctxLogger.With(fields...)
-	requestid, _ := c.Get(RequestIDKey)
-	if requestid != nil {
-		ctxLogger = ctxLogger.With(zap.String("requestid", requestid.(string)))
-	}
+func SetCtxLogger(c *gin.Context, ctxLogger *zap.Logger) {
 	c.Set(CtxLoggerKey, ctxLogger)
 }
 
 // CtxLogger get the ctxLogger in gin.Context
-func CtxLogger(c *gin.Context) *zap.Logger {
-	ctxLogger, _ := c.Get(CtxLoggerKey)
-	return ctxLogger.(*zap.Logger)
+func CtxLogger(c *gin.Context, fields ...zap.Field) *zap.Logger {
+	var ctxLogger *zap.Logger
+	ctxLoggerItf, _ := c.Get(CtxLoggerKey)
+	if ctxLoggerItf != nil {
+		ctxLogger = ctxLoggerItf.(*zap.Logger)
+	} else {
+		ctxLogger = CloneLogger().With(zap.String("requestid", CtxRequestID(c)))
+	}
+	if len(fields) > 0 {
+		ctxLogger = ctxLogger.With(fields...)
+	}
+	return ctxLogger
+}
+
+// CtxRequestID get requestid from context
+func CtxRequestID(c *gin.Context) string {
+	// first get from context
+	requestidItf, _ := c.Get(RequestIDKey)
+	if requestidItf != nil {
+		requestid := requestidItf.(string)
+		if requestid != "" {
+			return requestid
+		}
+	}
+	// if not then get request id from header
+	requestid := c.Request.Header.Get(RequestIDKey)
+	if requestid != "" {
+		return requestid
+	}
+	// else gen a request id
+	return uuid.NewV4().String()
+}
+
+// SetCtxRequestID set requestid for context
+func SetCtxRequestID(c *gin.Context, requestid string) {
+	// set in context
+	c.Set(RequestIDKey, requestid)
+	// set in header
+	c.Writer.Header().Set(RequestIDKey, requestid)
 }
