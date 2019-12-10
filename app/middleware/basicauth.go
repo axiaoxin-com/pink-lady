1544c746c6adee1b37ae23be1c0a07b346ec8bae
axiaoxin <254606826@qq.com>
2019-12-10 18:42:52 +0800

    支持启动时命令行指定配置文件位置和名称，去除viper中的cmd参数绑定

--- app/middleware/basicauth.go
+++ app/middleware/basicauth.go
@@ -5,9 +5,24 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	// DefaultAPIDocsUsername apidocs 默认用户名
+	DefaultAPIDocsUsername = "admin"
+	// DefaultAPIDocsPassword apidocs 默认密码
+	DefaultAPIDocsPassword = "!admin"
+)
+
 // BasicAuth use username and password in config to auth
 func BasicAuth() gin.HandlerFunc {
+	username := viper.GetString("apidocs.basicauth.username")
+	if username == "" {
+		username = DefaultAPIDocsUsername
+	}
+	password := viper.GetString("apidocs.basicauth.password")
+	if password == "" {
+		password = DefaultAPIDocsPassword
+	}
 	return gin.BasicAuth(gin.Accounts{
-		viper.GetString("apidocs.basicauth.username"): viper.GetString("apidocs.basicauth.password"),
+		username: password,
 	})
 }
