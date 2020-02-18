be2a2f25f21fc4153d559c5ebda2ef2517bf55d9
axiaoxin <254606826@qq.com>
2020-02-18 16:29:56 +0800

    release script update

--- script/release.sh
+++ script/release.sh
@@ -14,8 +14,12 @@ WARNING_FLAG="${YELLOW}!"
 ERROR_FLAG="${RED}!!"
 NOTICE_FLAG="${CYAN}❯"
 
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 # PATHS
-PROJECT_PATH=$(dirname $(dirname $(readlink -f $0)))
+PROJECT_PATH=$(dirname $(dirname $(realpath $0)))
 APP_PATH=${PROJECT_PATH}/app
 BUILD_PATH=${PROJECT_PATH}/build
 NOW=$(date "+%Y%m%d-%H%M%S")
@@ -28,13 +32,27 @@ BUILDING_FAILED=-3
 APIDOC_VERSION_PATTERN='// @version [0-9]+\.[0-9]+\.[0-9]+'
 CONST_VERSION_PATTERN='const VERSION = "[0-9]+\.[0-9]+\.[0-9]+"'
 
+
+OS=`uname`
+# $(replace_in_file pattern file)
+function replace_in_file() {
+    if [ "$OS" = 'Darwin' ]; then
+        # for MacOS
+        sed -i '' -E "$1" "$2"
+    else
+        # for Linux and Windows
+        sed -i'' "$1" "$2"
+    fi
+}
+
+
 # Bump version
 bumpVersion() {
     lastest_commit=$(git log --pretty=format:"%h %cd %d %s" -1)
     # check version
-    current_apidoc_version_line=($(grep -oE "${APIDOC_VERSION_PATTERN}" ${APP_PATH}/apis/init.go))
+    current_apidoc_version_line=($(grep -oE "${APIDOC_VERSION_PATTERN}" ${APP_PATH}/apis/apis.go))
     current_apidoc_version=${current_apidoc_version_line[2]}
-    current_const_version_line=$(grep -oE "${CONST_VERSION_PATTERN}" ${APP_PATH}/apis/init.go)
+    current_const_version_line=$(grep -oE "${CONST_VERSION_PATTERN}" ${APP_PATH}/apis/apis.go)
     current_const_version=$(echo "${current_const_version_line}" | grep -o '".*"' | sed 's/"//g')
     if [ "${current_apidoc_version}" != "${current_const_version}" ]; then
         echo -e "${ERROR_FLAG} ${RED}apidoc version ${current_apidoc_version} is not match with const version ${current_const_version}"
@@ -59,7 +77,7 @@ bumpVersion() {
 
     echo -e "${NOTICE_FLAG} Will set new version to be ${WHITE}${new_version}"
     # update version in apidoc and const
-    sed -i -r "s|${APIDOC_VERSION_PATTERN}|// @version ${new_version}|" ${APP_PATH}/apis/init.go && sed -i -r "s|${CONST_VERSION_PATTERN}|const VERSION = \"${new_version}\"|" ${APP_PATH}/apis/init.go
+    replace_in_file "s|${APIDOC_VERSION_PATTERN}|// @version ${new_version}|" ${APP_PATH}/apis/apis.go && replace_in_file "s|${CONST_VERSION_PATTERN}|const VERSION = \"${new_version}\"|" ${APP_PATH}/apis/apis.go
 }
 
 tests() {
@@ -85,7 +103,7 @@ build() {
     if [ "${binary_name}" != "" ]; then
         BINARY_NAME=${binary_name}
     fi
-    APIDOC_HOST=pink-lady
+    APIDOC_HOST=127.0.0.1:4869
     echo -ne "${QUESTION_FLAG} ${CYAN}Enter apidocs host[${WHITE}${APIDOC_HOST}${CYAN}]: "
     read apidoc_host
     if [ "${apidoc_host}" != "" ]; then
@@ -93,7 +111,7 @@ build() {
     fi
 
     # replace apidoc host
-    sed -i -r "s|// @host .+?|// @host ${APIDOC_HOST}|" ${APP_PATH}/apis/init.go
+    replace_in_file "s|// @host .+?|// @host ${APIDOC_HOST}|" ${APP_PATH}/apis/apis.go
 
     echo -e "${NOTICE_FLAG} Will build binary name to be ${WHITE}${BINARY_NAME} ${CYAN}with apidoc host ${WHITE}${APIDOC_HOST}"
 
@@ -104,9 +122,7 @@ build() {
         echo -e "${WARNING_FLAG} ${CYAN}No swag for generate API docs. You need to install it for auto generate the docs"
     else
         echo -e "${NOTICE_FLAG} Generating API docs..."
-        cd ${APP_PATH}
-        swag init -g apis/init.go
-        cd -
+        bash ${PROJECT_PATH}/script/gen_apidoc.sh
     fi
 
     # Building
@@ -139,7 +155,7 @@ commit() {
     echo -ne "${QUESTION_FLAG} ${CYAN}Do you want to commit this version bump to git[${WHITE}Y/n${CYAN}]: "
     read do_commit
     if [ "${do_commit}" == "" ] || [ "${do_commit}" == "Y" ]; then
-        git add ${APP_PATH}/docs ${APP_PATH}/apis/init.go
+        git add ${APP_PATH}/docs ${APP_PATH}/apis/apis.go
         git commit -m "Bump version ${current_version} -> ${new_version}"
         echo -ne "${QUESTION_FLAG} ${CYAN}Do you want to tag this version bump to git[${WHITE}Y/n${CYAN}]: "
         read do_tag
