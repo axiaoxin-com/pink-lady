a50a12743003d1e7a2da42bbf1bcc5e724ddc8f0
axiaoxin <254606826@qq.com>
2020-02-18 11:36:51 +0800

    update new proj script

--- script/new-project.sh
+++ script/new-project.sh
@@ -5,8 +5,24 @@ WHITE="\033[1;37m"
 NOTICE_FLAG="${CYAN}❯"
 QUESTION_FLAG="${GREEN}?"
 
+OS=`uname`
+# $(replace_in_file pattern file)
+function replace_in_file() {
+    if [ "$OS" = 'Darwin' ]; then
+        # for MacOS
+        sed -i '' -e "$1" "$2"
+    else
+        # for Linux and Windows
+        sed -i'' -e "$1" "$2"
+    fi
+}
+
 main() {
     gopath=`go env GOPATH`
+    if [ $? = 127 ]; then
+        echo "GOPATH not exists"
+        exit -1
+    fi
     echo -e "${NOTICE_FLAG} New project will be create in ${WHITE}${gopath}/src/"
     echo -ne "${QUESTION_FLAG} ${CYAN}Enter your new project full name${CYAN}: "
     read projname
@@ -19,15 +35,15 @@ main() {
     # replace project name
     echo -e "${NOTICE_FLAG} Generating the project..."
     cd ${gopath}/src/${projname} && rm -rf .git && cp ${gopath}/src/${projname}/app/config.toml.example ${gopath}/src/${projname}/app/config.toml
-    sed -i "s|github.com/axiaoxin/pink-lady|${projname}|g"  `grep "github.com/axiaoxin/pink-lady" --include ".travis.yml" --include "*.go" --include "go.*" -rl .`
+    replace_in_file "s|github.com/axiaoxin/pink-lady|${projname}|g"  `grep "github.com/axiaoxin/pink-lady" --include ".travis.yml" --include "*.go" --include "go.*" -rl .`
 
     # remove demo
     if [ "${rmdemo}" == "n" ] || [ "${rmdemo}" == "N" ]; then
         rm -rf app/apis/demo
         rm -rf app/services/demo
         rm -rf app/models/demo
-        sed -i "/demo routes start/,/demo routes end/d" ${gopath}/src/${projname}/app/apis/routes.go
-        sed -i '/app\/apis\/demo"$/d' ${gopath}/src/${projname}/app/apis/routes.go
+        replace_in_file "/demo routes start/,/demo routes end/d"  ${gopath}/src/${projname}/app/apis/routes.go
+        replace_in_file '/app\/apis\/demo"$/d' ${gopath}/src/${projname}/app/apis/routes.go
     fi
     echo -e "${NOTICE_FLAG} Create project ${projname} in ${gopath}/src succeed."
 
@@ -36,7 +52,7 @@ main() {
     read initgit
     if [ "${initgit}" == "y" ] || [ "${rmdemo}" == "Y" ]; then
         cd ${gopath}/src/${projname} && git init && git add . && git commit -m "init project with pink-lady"
-        cp ${gopath}/src/${projname}/misc/pre-commit.linux.githook ${gopath}/src/${projname}/.git/hooks/pre-commit
+        cp ${gopath}/src/${projname}/script/pre-commit.githook ${gopath}/src/${projname}/.git/hooks/pre-commit
         chmod +x ${gopath}/src/${projname}/.git/hooks/pre-commit
     fi
 }
