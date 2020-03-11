72ca68079008de3b1b3735bffe6a6ab53e83a37e
axiaoxin <254606826@qq.com>
2020-03-11 22:14:13 +0800

    update tests

--- app/logging/logging_test.go
+++ app/logging/logging_test.go
@@ -14,4 +14,14 @@ func TestInterfaces(t *testing.T) {
 	Warn("msg")
 	Error("中文")
 	Error("with error field", zap.Error(errors.New("iamerror")))
+
+	Debugf("%s", "asf")
+	Infof("%s", "asf")
+	Warnf("%s", "asf")
+	Errorf("%s", "asf")
+
+	Debugw("asf", "k1", "v1")
+	Infow("asf", "k1", "v1")
+	Warnw("asf", "k1", "v1")
+	Errorw("asf", "k1", "v1")
 }
