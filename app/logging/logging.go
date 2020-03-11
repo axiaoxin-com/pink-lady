81055b235a158468ea7fb623e741aef8ff374c06
axiaoxin <254606826@qq.com>
2020-03-11 16:05:21 +0800

    sugar logging

--- app/logging/logging.go
+++ app/logging/logging.go
@@ -1,37 +1,89 @@
 // Package logging provides ...
 package logging
 
-import (
-	"github.com/getsentry/sentry-go"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-)
-
 // Debug 记录debug级别的日志
-func Debug(msg string, fields ...zap.Field) {
-	defer Logger.Sync()
-	Logger.Debug(msg, fields...)
+// logging.Debug("abc", 123)
+func Debug(args ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Debug(args...)
 }
 
 // Info 记录info级别的日志
-func Info(msg string, fields ...zap.Field) {
-	defer Logger.Sync()
-	Logger.Info(msg, fields...)
+func Info(args ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Info(args...)
 }
 
 // Warn 记录warn级别的日志
-func Warn(msg string, fields ...zap.Field) {
-	defer Logger.Sync()
-	Logger.Warn(msg, fields...)
+func Warn(args ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Warn(args...)
 }
 
-// Error 记录Error级别的日志，如果fields中有zap.Error则上报的sentry
-func Error(msg string, fields ...zap.Field) {
-	defer Logger.Sync()
-	Logger.Error(msg, fields...)
-	for _, field := range fields {
-		if field.Type == zapcore.ErrorType {
-			sentry.CaptureException(field.Interface.(error))
-		}
-	}
+// Error 记录Error级别的日志
+func Error(args ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Error(args...)
+}
+
+// Debugf 模板字符串记录debug级别的日志
+// logging.Debugf("str:%s", "abd")
+func Debugf(template string, args ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Debugf(template, args...)
+}
+
+// Infof 模板字符串记录info级别的日志
+func Infof(template string, args ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Infof(template, args...)
+}
+
+// Warnf 模板字符串记录warn级别的日志
+func Warnf(template string, args ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Warnf(template, args...)
+}
+
+// Errorf 模板字符串记录debug级别的日志
+func Errorf(template string, args ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Errorf(template, args...)
+}
+
+// Debugw kv记录debug级别的日志
+// logging.Debugw("msg", "k1", "v1", "k2", "v2")
+func Debugw(msg string, keysAndValues ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Debugw(msg, keysAndValues...)
+}
+
+// Infow kv记录info级别的日志
+func Infow(msg string, keysAndValues ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Infow(msg, keysAndValues...)
+}
+
+// Warnw kv记录warn级别的日志
+func Warnw(msg string, keysAndValues ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Warnw(msg, keysAndValues...)
+}
+
+// Errorw kv记录error级别的日志
+func Errorw(msg string, keysAndValues ...interface{}) {
+	slogger := Logger.Sugar()
+	defer slogger.Sync()
+	slogger.Errorw(msg, keysAndValues...)
 }
