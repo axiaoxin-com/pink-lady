4f788bac05b0e14a284bed2b6f7c2065709e0af8
axiaoxin <254606826@qq.com>
2020-11-23 10:39:21 +0800

    services fix

--- src/services/services_health.go
+++ src/services/services_health.go
@@ -15,17 +15,15 @@ import (
 
 // CheckMySQL 检查 mysql 服务状态
 func CheckMySQL(ctx context.Context) map[string]string {
-	// 检查本地 mysql
-	localhostMySQLStatus := "ok"
-	if localhostMySQL, err := GormMySQL("localhost"); err != nil {
-		localhostMySQLStatus = err.Error()
-	} else if sqlDB, err := localhostMySQL.DB(); err != nil {
-		localhostMySQLStatus = err.Error()
+	// 检查默认 mysql
+	defaultMySQLStatus := "ok"
+	if sqlDB, err := DB(ctx).DB(); err != nil {
+		defaultMySQLStatus = err.Error()
 	} else if err := sqlDB.Ping(); err != nil {
-		localhostMySQLStatus = err.Error()
+		defaultMySQLStatus = err.Error()
 	}
 	return map[string]string{
-		"localhost": localhostMySQLStatus,
+		"default DB": defaultMySQLStatus,
 	}
 }
 
