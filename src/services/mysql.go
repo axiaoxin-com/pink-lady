f04e60aca90510b4ef110b53747573e2c4470823
axiaoxin <254606826@qq.com>
2020-11-26 18:16:02 +0800

    env 支持 unittest 时使用 sqlite3

--- src/services/mysql.go
+++ src/services/mysql.go
@@ -9,11 +9,26 @@ import (
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 // DB 获取带有ctx 和自定义 logger 的 gorm db 实例
 func DB(ctx context.Context) *gorm.DB {
 	env := viper.GetString("env")
+	if env == "unittest" {
+		dbname := viper.GetString("unittest.dbname")
+		if dbname == "" {
+			dbname = "/tmp/pinklady_test.db"
+		}
+		db, err := goutils.NewGormSQLite3(goutils.DBConfig{DBName: dbname, GormConfig: &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		}})
+		if err != nil {
+			panic(err)
+		}
+		return db
+	}
+
 	db, err := goutils.GormMySQL(env)
 	if err != nil {
 		panic(env + " get gorm mysql instance error:" + err.Error())
