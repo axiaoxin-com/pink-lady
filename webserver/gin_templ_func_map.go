5c69eb648935b2564a688d6d95a76424a0e38268
axiaoxin <254606826@qq.com>
2022-08-10 16:29:45 +0800

    update func map

--- webserver/gin_templ_func_map.go
+++ webserver/gin_templ_func_map.go
@@ -5,8 +5,12 @@ package webserver
 import (
 	"html/template"
 	"strings"
+	"time"
 
+	"github.com/axiaoxin-com/logging"
 	"github.com/chai2010/gettext-go"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/gomarkdown/markdown"
 	"github.com/microcosm-cc/bluemonday"
 )
@@ -23,7 +27,6 @@ var TemplFuncs = map[string]interface{}{
 		html = bluemonday.UGCPolicy().SanitizeBytes(html)
 		return template.HTML(html)
 	},
-
 	"_int_sum": func(ints ...int) int {
 		sum := 0
 		for _, i := range ints {
@@ -31,7 +34,52 @@ var TemplFuncs = map[string]interface{}{
 		}
 		return sum
 	},
+	"_int_sub": func(ints ...int) int {
+		if len(ints) == 0 {
+			return 0
+		}
+		sub := ints[0]
+		for _, i := range ints[1:] {
+			sub -= i
+		}
+		return sub
+	},
+	"_percent": func(i1, i2 int) int {
+		if i2 == 0 {
+			return 0
+		}
+		return int(float64(i1) / float64(i2) * 100)
+	},
 	"_str_repeat": func(s string, count int) string {
 		return strings.Repeat(s, count)
+	},
+	"_last_page_offset": func(total int, limit int) int {
+		return total / limit * limit
+	},
+	"_str_cut": func(s string, cut int) string {
+		return string([]rune(s)[:cut])
+	},
+	"_pbts_format": func(pbts *timestamp.Timestamp, format string) string {
+		gotime, err := ptypes.Timestamp(pbts)
+		if err != nil {
+			logging.Error(nil, err.Error())
+			return ""
+		}
+		loc, err := time.LoadLocation("Asia/Shanghai")
+		if err != nil {
+			logging.Error(nil, err.Error())
+		} else {
+			return gotime.In(loc).Format(format)
+		}
+		return gotime.Local().Format(format)
+	},
+	"_time_format": func(t time.Time, format string) string {
+		loc, err := time.LoadLocation("Asia/Shanghai")
+		if err != nil {
+			logging.Error(nil, err.Error())
+		} else {
+			return t.In(loc).Format(format)
+		}
+		return t.Local().Format(format)
 	},
 }
