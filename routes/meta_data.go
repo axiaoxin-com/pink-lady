9fbf8e7873e1c75573fb9d75f4478bdc62c663b1
axiaoxin <254606826@qq.com>
2023-12-28 15:31:06 +0800

    静态文件绝对路径、统计js

--- routes/meta_data.go
+++ routes/meta_data.go
@@ -50,6 +50,8 @@ type MetaData struct {
 	FlatpagesEnable  bool
 	FlatpagesNavName string
 	FlatpagesNavPath string
+	BaiduTongJiID    string
+	GtagID           string
 }
 
 // NewMetaData 返回页面元数据
@@ -87,11 +89,13 @@ func NewMetaData(c *gin.Context, title string) (m *MetaData) {
 		Beian:            viper.GetString("server.beian"),
 		AuthorName:       viper.GetString("author.name"),
 		AuthorURL:        viper.GetString("author.url"),
-		StaticsURL:       viper.GetString("statics.url"),
+		StaticsURL:       hostURL + "/" + viper.GetString("statics.url"),
 		StaticsSelfhost:  viper.GetBool("statics.selfhost"),
 		FlatpagesEnable:  viper.GetBool("flatpages.enable"),
 		FlatpagesNavName: viper.GetString("flatpages.nav_name"),
 		FlatpagesNavPath: viper.GetString("flatpages.nav_path"),
+		BaiduTongJiID:    viper.GetString("server.baidu_tongji_id"),
+		GtagID:           viper.GetString("server.gtag_id"),
 	}
 
 	logging.Debugf(ctx, "NewMetaData MetaData:%+v", *m)
