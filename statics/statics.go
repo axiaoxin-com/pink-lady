0c8bc644a4ec0f7746b6a3ef103efc25a27d5443
axiaoxin <254606826@qq.com>
2025-01-02 21:13:13 +0800

    font dir fix

--- statics/statics.go
+++ statics/statics.go
@@ -6,6 +6,6 @@ import "embed"
 // Files 静态文件资源
 //
 //go:embed robots.txt
-//go:embed css/* font/* html/* img/* js/*
+//go:embed css/* sitemap/* html/* img/* js/*
 //go:embed flatpages/*
 var Files embed.FS
